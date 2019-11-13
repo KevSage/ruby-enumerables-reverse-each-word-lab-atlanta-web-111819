f422b0fc5bcdbbc6a9c3a8639cb7673366aea361
Kevin Sage <ksagedeveloper@gmail.com>
2019-11-13 22:29:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 require 'pry'
 def reverse_each_word(sentence)
   split_array = sentence.split(' ')
-  split_array = split_array.map(|letter| letter.reverse)
+  split_array = split_array.map{|letter| letter.reverse}
 pp split_array
 end
